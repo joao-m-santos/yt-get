71d366db3d62fa0bcb64c869303ff19195d6c589
joao-m-santos <joao_santos9@proton.me>
2025-07-20 12:58:52 +0200

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'yt-get',
+      script: '.output/server/index.mjs',
+      interpreter: 'bun',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
